9b35ed7c5d7e9105de610537df97b8b48273ec46
Draven Schilling <Dravenschilling@gmail.com>
2016-02-02 20:49:06 -0600

    bugfixes and calculations added

--- get_from_database.php
+++ get_from_database.php
@@ -10,17 +10,75 @@ if(isset($_POST['search'])){
 
 // Creates querys for the database
 //do all calculations based on these query's then display data
-	$allQuery = "SELECT autoDefence, autoShoot, categoryA, categoryAScore, categoryB, categoryBScore, categoryC, categoryCScore, categoryD, categoryDScore, lowBarScore, lowGoalShots, highGoalShots, comments FROM match WHERE team = $team";
+	$allQuery = "SELECT autoDefence, highGoalAutoShotsMade, lowGoalAutoShotsMade, categoryA, categoryAScore, categoryB, categoryBScore, categoryC, categoryCScore, categoryD, categoryDScore, lowBarScore, lowGoalShots, highGoalShots, comments FROM match WHERE team = $team";
 	$portcullisQuery = "SELECT categoryAScore FROM match WHERE team = $team AND categoryA ='Portcullis'";
-	$chevalDeFriseQuery = "SELECT categoryAScore FROM match WHERE team = $team AND categoryA ='Cheval de Frise'";
-	$moatQuery = "SELECT categoryBScore FROM match WHERE team = $team AND categoryB ='Moat'";
-	$rampartsQuery = "SELECT categoryBScore FROM match WHERE team = $team AND categoryB ='Ramparts'";
-	$drawbridgeQuery = "SELECT categoryCScore FROM match WHERE team = $team AND categoryC ='drawbridge'";
-	$sallyPortQuery = "SELECT categoryCScore FROM match WHERE team = $team AND categoryC ='Sally Port'";
-	$lowBarQuery = "SELECT lowBarScore FROM match WHERE team = $team";
-	$lowGoalQuery = "SELECT lowGoalShots FROM match WHERE team = $team";
-	$highGoalQuery = "SELECT highGoalShots FROM match WHERE team = $team";
+	$chevalDeFriseQuery = "SELECT SUM(categoryAScore) AS ChevalCrosses FROM match WHERE team = $team AND categoryA ='Cheval de Frise'";
+	$moatQuery = "SELECT SUM(categoryBScore) AS MoatCrosses FROM match WHERE team = $team AND categoryB ='Moat'";
+	$rampartsQuery = "SELECT SUM(categoryBScore) AS RampartCrosses FROM match WHERE team = $team AND categoryB ='Ramparts'";
+	$drawbridgeQuery = "SELECT SUM(categoryCScore) AS DrawbridgeCrosses FROM match WHERE team = $team AND categoryC ='Drawbridge'";
+	$sallyPortQuery = "SELECT SUM(categoryCScore) AS SallyPortCrosses FROM match WHERE team = $team AND categoryC ='Sally Port'";
+	$rockWallQuery = "SELECT SUM(categoryDScore) AS RockWallCrosses FROM match WHERE team = $team AND categoryD ='Rock Wall'";
+	$roughTerrainQuery = "SELECT SUM(categoryDScore) AS RoughTerrainCrosses FROM match WHERE team = $team AND categoryD ='Rough Terrain'";
+	$lowBarQuery = "SELECT SUM(lowBarScore) AS LowBarCrosses FROM match WHERE team = $team";
+	$lowGoalHitsQuery = "SELECT SUM(lowGoalShots) AS LowGoalHits FROM match WHERE team = $team";
+	$lowGoalMissesQuery = "SELECT SUM(missedLowGoalShots) AS LowGoalMisses FROM match WHERE team = $team";
+	$highGoalHitsQuery = "SELECT SUM(highGoalShots) AS HighGoalHits FROM match WHERE team = $team";
+	$highGoalMissesQuery = "SELECT SUM(highGoalShots) AS HighGoalMisses FROM match WHERE team = $team";
 	$numMatchesQuery = "SELECT team FROM match WHERE team = $team";
+	
+
+// Query Calculations //
+
+// Low goal Accuracy Calculations //	
+	$lgha = mysql_fetch_assoc($lowGoalHitsQuery); 
+	$lowGoalHits = $lgha['LowGoalHits']; // Num low goal shots made
+	$lgma = mysql_fetch_assoc($lowGoalMissesQuery); 
+	$lowGoalMisses = $lgma['LowGoalMisses'];
+	$lowGoalAccuracy = (($lowGoalHits/($lowGoalHits+$lowGoalMisses))*100); //double value of hit ratio
+	$lowGoalAccuracy = round($lowGoalAccuracy, 1, PHP_ROUND_HALF_UP);
+// High Goal Accuracy Calculations //
+	$hgha = mysql_fetch_assoc($highGoalHitsQuery); 
+	$highGoalHits = $lgha['HighGoalHits']; // Num high goal shots made
+	$hgma = mysql_fetch_assoc($hightGoalMissesQuery); 
+	$highGoalMisses = $lgma['HighGoalMisses'];
+	$highGoalAccuracy = (($highGoalHits/($highGoalHits+$highGoalMisses))*100); //double value of hit ratio
+	$highGoalAccuracy = round($highGoalAccuracy, 1, PHP_ROUND_HALF_UP);
+
+	//todo
+	//num matches calculations
+	//scale tower accuracy (maybe)
+	//
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Get a response from the database by sending the connection and the query
 	$allSearch = @mysqli_query($dbc, $allQuery);
@@ -43,12 +101,14 @@ if(isset($_POST['search'])){
 		<td align="left"><b>Low Goal Shots Made</b></td>
 		<td align="left"><b>High Goal Shots Made</b></td>
 		<td align="left"><b>Comments</b></tr>';
-
+//"left""right""left""right"
 // mysqli_fetch_array will return a row of data from the query
 // until no further data is available
-		while($row = mysqli_fetch_array($response)){
+		while($row = mysqli_fetch_array($allSearch)){
 			echo '<tr>
-			<td align="left">' . $row['autoShoot'] . '</td>
+			<td align="left">' . $row['autoDefence'] . '</td>
+			<td align="left">' . $row['highGoalAutoShotsMade'] . '</td>
+			<td align="left">' . $row['lowGoalAutoShotsMade'] . '</td>
 			<td align="left">' . $row['categoryA'] . '</td>
 			<td align="left">' . $row['categoryAScore'] . '</td>
 			<td align="left">' . $row['categoryB'] . '</td>
@@ -68,9 +128,9 @@ if(isset($_POST['search'])){
 		echo "Couldn't issue database query<br />";
 		echo mysqli_error($dbc);
 	}
-
-	
 // Close connection to the database
 mysqli_close($dbc);
+// mysqli_num_rows()
+//mysqli_fetch_lengths() Returns an array if integer that represents the size of each field (column)
 
 ?>
