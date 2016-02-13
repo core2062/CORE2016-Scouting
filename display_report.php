9bf49b27fbc3ec8d9f1b4d31d62c1078203469d2
Draven Schilling <Dravenschilling@gmail.com>
2016-02-13 15:59:02 -0600

    Syntax fixes and creation of basic html report

---
+++ display_report.php
@@ -0,0 +1,173 @@
+<?php
+include 'get_from_database.php';
+
+// wait till it's posted
+if(isset($_POST['search'])){
+
+// Which team is being searched for
+    $redTeam1 = trim($_POST['redTeam1']);
+    $redTeam2 = trim($_POST['redTeam2']);
+    $redTeam3 = trim($_POST['redTeam3']);
+    $blueTeam4 = trim($_POST['blueTeam1']);
+    $blueTeam5 = trim($_POST['blueTeam2']);
+    $blueTeam6 = trim($_POST['blueTeam3']);
+
+// Create team objects //
+// access by $obj->var & $obj->method()
+	$redTeam1Object = new teamReport($redTeam1);
+	$redTeam2Object = new teamReport($redTeam2);
+	$redTeam3Object = new teamReport($redTeam3);
+	$blueTeam1Object = new teamReport($blueTeam1);
+	$blueTeam2Object = new teamReport($blueTeam2);
+	$blueTeam3Object = new teamReport($blueTeam3);
+
+
+	echo "<b>$team</b>"
+	?>
+	<table align="left" cellspacing="5" cellpadding="8">
+		<tr>
+			<td align="left"><b>Team</b></td>
+			<td align="left"><b><?php echo $redTeam1Object->team ?></b></td>
+			<td align="left"><b><?php echo $redTeam2Object->team ?></b></td>
+			<td align="left"><b><?php echo $redTeam3Object->team ?></b></td>
+			<td align="left"><b><?php echo $blueTeam1Object->team ?></b></td>
+			<td align="left"><b><?php echo $blueTeam2Object->team ?></b></td>
+			<td align="left"><b><?php echo $blueTeam3Object->team ?></b></td>
+		</tr>
+		<tr>
+			<td align="left"><b>Portcullis Crosses</b></td>
+			<td align="left"><b><?php echo $redTeam1Object->portcullis ?></b></td>
+			<td align="left"><b><?php echo $redTeam2Object->portcullis ?></b></td>
+			<td align="left"><b><?php echo $redTeam3Object->portcullis ?></b></td>
+			<td align="left"><b><?php echo $blueTeam1Object->portcullis ?></b></td>
+			<td align="left"><b><?php echo $blueTeam2Object->portcullis ?></b></td>
+			<td align="left"><b><?php echo $blueTeam3Object->portcullis ?></b></td>
+		</tr>
+		<tr>
+			<td align="left"><b>Cheval de Frise</b></td>
+			<td align="left"><b><?php echo $redTeam1Object->chevalDeFrise ?></b></td>
+			<td align="left"><b><?php echo $redTeam2Object->chevalDeFrise ?></b></td>
+			<td align="left"><b><?php echo $redTeam3Object->chevalDeFrise ?></b></td>
+			<td align="left"><b><?php echo $blueTeam1Object->chevalDeFrise ?></b></td>
+			<td align="left"><b><?php echo $blueTeam2Object->chevalDeFrise ?></b></td>
+			<td align="left"><b><?php echo $blueTeam3Object->chevalDeFrise ?></b></td>
+		</tr>
+		<tr>
+			<td align="left"><b>Moat Crosses</b></td>
+			<td align="left"><b><?php echo $redTeam1Object->moat ?></b></td>
+			<td align="left"><b><?php echo $redTeam2Object->moat ?></b></td>
+			<td align="left"><b><?php echo $redTeam3Object->moat ?></b></td>
+			<td align="left"><b><?php echo $blueTeam1Object->moat ?></b></td>
+			<td align="left"><b><?php echo $blueTeam2Object->moat ?></b></td>
+			<td align="left"><b><?php echo $blueTeam3Object->moat ?></b></td>
+		</tr>
+		<tr>
+			<td align="left"><b>Rampart Crosses</b></td>
+			<td align="left"><b><?php echo $redTeam1Object->ramparts ?></b></td>
+			<td align="left"><b><?php echo $redTeam2Object->ramparts ?></b></td>
+			<td align="left"><b><?php echo $redTeam3Object->ramparts ?></b></td>
+			<td align="left"><b><?php echo $blueTeam1Object->ramparts ?></b></td>
+			<td align="left"><b><?php echo $blueTeam2Object->ramparts ?></b></td>
+			<td align="left"><b><?php echo $blueTeam3Object->ramparts ?></b></td>
+		</tr>
+		<tr>
+			<td align="left"><b>Drawbridge Crosses</b></td>
+			<td align="left"><b><?php echo $redTeam1Object->drawbridge ?></b></td>
+			<td align="left"><b><?php echo $redTeam2Object->drawbridge ?></b></td>
+			<td align="left"><b><?php echo $redTeam3Object->drawbridge ?></b></td>
+			<td align="left"><b><?php echo $blueTeam1Object->drawbridge ?></b></td>
+			<td align="left"><b><?php echo $blueTeam2Object->drawbridge ?></b></td>
+			<td align="left"><b><?php echo $blueTeam3Object->drawbridge ?></b></td>
+		</tr>
+		<tr>
+			<td align="left"><b>Sally Port Crosses</b></td>
+			<td align="left"><b><?php echo $redTeam1Object->sallyPort ?></b></td>
+			<td align="left"><b><?php echo $redTeam2Object->sallyPort ?></b></td>
+			<td align="left"><b><?php echo $redTeam3Object->sallyPort ?></b></td>
+			<td align="left"><b><?php echo $blueTeam1Object->sallyPort ?></b></td>
+			<td align="left"><b><?php echo $blueTeam2Object->sallyPort ?></b></td>
+			<td align="left"><b><?php echo $blueTeam3Object->sallyPort ?></b></td>
+		</tr>
+		<tr>
+			<td align="left"><b>Rock Wall Crosses</b></td>
+			<td align="left"><b><?php echo $redTeam1Object->rockWall ?></b></td>
+			<td align="left"><b><?php echo $redTeam2Object->rockWall ?></b></td>
+			<td align="left"><b><?php echo $redTeam3Object->rockWall ?></b></td>
+			<td align="left"><b><?php echo $blueTeam1Object->rockWall ?></b></td>
+			<td align="left"><b><?php echo $blueTeam2Object->rockWall ?></b></td>
+			<td align="left"><b><?php echo $blueTeam3Object->rockWall ?></b></td>
+		</tr>
+		<tr>
+			<td align="left"><b>Rough Terrain Crosses</b></td>
+			<td align="left"><b><?php echo $redTeam1Object->roughTerrain ?></b></td>
+			<td align="left"><b><?php echo $redTeam2Object->roughTerrain ?></b></td>
+			<td align="left"><b><?php echo $redTeam3Object->roughTerrain ?></b></td>
+			<td align="left"><b><?php echo $blueTeam1Object->roughTerrain ?></b></td>
+			<td align="left"><b><?php echo $blueTeam2Object->roughTerrain ?></b></td>
+			<td align="left"><b><?php echo $blueTeam3Object->roughTerrain ?></b></td>
+		</tr>
+		<tr>
+			<td align="left"><b>Low Bar Crosses</b></td>
+			<td align="left"><b><?php echo $redTeam1Object->lowBar ?></b></td>
+			<td align="left"><b><?php echo $redTeam2Object->lowBar ?></b></td>
+			<td align="left"><b><?php echo $redTeam3Object->lowBar ?></b></td>
+			<td align="left"><b><?php echo $blueTeam1Object->lowBar ?></b></td>
+			<td align="left"><b><?php echo $blueTeam2Object->lowBar ?></b></td>
+			<td align="left"><b><?php echo $blueTeam3Object->lowBar ?></b></td>
+		</tr>
+		<tr>
+			<td align="left"><b>High Goal Shots Made</b></td>
+			<td align="left"><b><?php echo $redTeam1Object->highGoalAccuracyFraction ?></b></td>
+			<td align="left"><b><?php echo $redTeam2Object->highGoalAccuracyFraction ?></b></td>
+			<td align="left"><b><?php echo $redTeam3Object->highGoalAccuracyFraction ?></b></td>
+			<td align="left"><b><?php echo $blueTeam1Object->highGoalAccuracyFraction ?></b></td>
+			<td align="left"><b><?php echo $blueTeam2Object->highGoalAccuracyFraction ?></b></td>
+			<td align="left"><b><?php echo $blueTeam3Object->highGoalAccuracyFraction ?></b></td>
+		</tr>
+		<tr>
+			<td align="left"><b>High Goal Accuracy</b></td>
+			<td align="left"><b><?php echo $redTeam1Object->highGoalAccuracyPercent ?></b></td>
+			<td align="left"><b><?php echo $redTeam2Object->highGoalAccuracyPercent ?></b></td>
+			<td align="left"><b><?php echo $redTeam3Object->highGoalAccuracyPercent ?></b></td>
+			<td align="left"><b><?php echo $blueTeam1Object->highGoalAccuracyPercent ?></b></td>
+			<td align="left"><b><?php echo $blueTeam2Object->highGoalAccuracyPercent ?></b></td>
+			<td align="left"><b><?php echo $blueTeam3Object->highGoalAccuracyPercent ?></b></td>
+		</tr>
+		<tr>
+			<td align="left"><b>Low Goal Shots Made</b></td>
+			<td align="left"><b><?php echo $redTeam1Object->lowGoalAccuracyPercent ?></b></td>
+			<td align="left"><b><?php echo $redTeam2Object->lowGoalAccuracyPercent ?></b></td>
+			<td align="left"><b><?php echo $redTeam3Object->lowGoalAccuracyPercent ?></b></td>
+			<td align="left"><b><?php echo $blueTeam1Object->lowGoalAccuracyPercent ?></b></td>
+			<td align="left"><b><?php echo $blueTeam2Object->lowGoalAccuracyPercent ?></b></td>
+			<td align="left"><b><?php echo $blueTeam3Object->lowGoalAccuracyPercent ?></b></td>
+		</tr>
+		<tr>
+			<td align="left"><b>Low Goal Accuracy</b></td>
+			<td align="left"><b><?php echo $redTeam1Object->lowGoalAccuracyPercent ?></b></td>
+			<td align="left"><b><?php echo $redTeam2Object->lowGoalAccuracyPercent ?></b></td>
+			<td align="left"><b><?php echo $redTeam3Object->lowGoalAccuracyPercent ?></b></td>
+			<td align="left"><b><?php echo $blueTeam1Object->lowGoalAccuracyPercent ?></b></td>
+			<td align="left"><b><?php echo $blueTeam2Object->lowGoalAccuracyPercent ?></b></td>
+			<td align="left"><b><?php echo $blueTeam3Object->lowGoalAccuracyPercent ?></b></td>
+		</tr>
+		<tr>
+			<td align="left"><b>Challenging</b></td>
+			<td align="left"><b><?php echo $redTeam1Object->highGoalAccuracyPercent ?></b></td>
+			<td align="left"><b><?php echo $redTeam2Object->highGoalAccuracyPercent ?></b></td>
+			<td align="left"><b><?php echo $redTeam3Object->highGoalAccuracyPercent ?></b></td>
+			<td align="left"><b><?php echo $blueTeam1Object->highGoalAccuracyPercent ?></b></td>
+			<td align="left"><b><?php echo $blueTeam2Object->highGoalAccuracyPercent ?></b></td>
+			<td align="left"><b><?php echo $blueTeam3Object->highGoalAccuracyPercent ?></b></td>
+		</tr>
+		<tr>
+			<td align="left"><b>Scaling</b></td>
+			<td align="left"><b><?php echo $redTeam1Object->highGoalAccuracyPercent ?></b></td>
+			<td align="left"><b><?php echo $redTeam2Object->highGoalAccuracyPercent ?></b></td>
+			<td align="left"><b><?php echo $redTeam3Object->highGoalAccuracyPercent ?></b></td>
+			<td align="left"><b><?php echo $blueTeam1Object->highGoalAccuracyPercent ?></b></td>
+			<td align="left"><b><?php echo $blueTeam2Object->highGoalAccuracyPercent ?></b></td>
+			<td align="left"><b><?php echo $blueTeam3Object->highGoalAccuracyPercent ?></b></td>
+		</tr>
+	</table>;
+}
--- get_from_database.php
+++ get_from_database.php
@@ -1,112 +1,115 @@
 <?php
-// Get a connection for the database
-require_once('../mysqli_connect.php');
 
 class teamReport {
 	public $team;
     function __construct($team) {
     	$this->team = $team;
 	}
+	// Get a connection for the database
+	require_once('../mysqli_connect.php');
+
 	// Creates querys for the database
 	//do all calculations based on these query's then display data
 	private $defenceAQuery = "SELECT matchNum FROM match";
 	//$allQuery = "SELECT autoDefence, highGoalAutoShotsMade, lowGoalAutoShotsMade, categoryA, categoryAScore, categoryB, categoryBScore, categoryC, categoryCScore, categoryD, categoryDScore, lowBarScore, lowGoalShots, highGoalShots, comments FROM match WHERE team = $team";
-	private $portcullisQuery = "SELECT SUM(categoryAScore) AS PortcullisCrosses FROM match WHERE team = $team AND categoryA ='Portcullis'";
-	private $chevalDeFriseQuery = "SELECT SUM(categoryAScore) AS ChevalCrosses FROM match WHERE team = $team AND categoryA ='Cheval de Frise'";
-	private $moatQuery = "SELECT SUM(categoryBScore) AS MoatCrosses FROM match WHERE team = $team AND categoryB ='Moat'";
-	private $rampartsQuery = "SELECT SUM(categoryBScore) AS RampartCrosses FROM match WHERE team = $team AND categoryB ='Ramparts'";
-	private $drawbridgeQuery = "SELECT SUM(categoryCScore) AS DrawbridgeCrosses FROM match WHERE team = $team AND categoryC ='Drawbridge'";
-	private $sallyPortQuery = "SELECT SUM(categoryCScore) AS SallyPortCrosses FROM match WHERE team = $team AND categoryC ='Sally Port'";
-	private $rockWallQuery = "SELECT SUM(categoryDScore) AS RockWallCrosses FROM match WHERE team = $team AND categoryD ='Rock Wall'";
-	private $roughTerrainQuery = "SELECT SUM(categoryDScore) AS RoughTerrainCrosses FROM match WHERE team = $team AND categoryD ='Rough Terrain'";
-	private $lowBarQuery = "SELECT SUM(lowBarScore) AS LowBarCrosses FROM match WHERE team = $team";
-	private $lowGoalHitsQuery = "SELECT SUM(lowGoalShots) AS LowGoalHits FROM match WHERE team = $team";
-	private $lowGoalMissesQuery = "SELECT SUM(missedLowGoalShots) AS LowGoalMisses FROM match WHERE team = $team";
-	private $highGoalHitsQuery = "SELECT SUM(highGoalShots) AS HighGoalHits FROM match WHERE team = $team";
-	private $highGoalMissesQuery = "SELECT SUM(highGoalShots) AS HighGoalMisses FROM match WHERE team = $team";
-	private $numMatchesQuery = "SELECT team FROM match WHERE team = team";
-	private $towerScaleQuery = "SELECT scaleTower FROM match WHERE team = $team AND scaleTower = 'Yes'";
-	private $towerChallengeQuery = "SELECT challengeTower FROM match WHERE team = $team AND challengeTower = 'Yes'";
-	private $commentsQuery = "SELECT comments FROM match WHERE team = $team AND comments != 'N/A'";
+	private $portcullisQuery = "SELECT SUM(categoryAScore) AS PortcullisCrosses FROM match WHERE team = {$this->team} AND categoryA ='Portcullis'";
+	private $chevalDeFriseQuery = "SELECT SUM(categoryAScore) AS ChevalCrosses FROM match WHERE team = {$this->team} AND categoryA ='Cheval de Frise'";
+	private $moatQuery = "SELECT SUM(categoryBScore) AS MoatCrosses FROM match WHERE team = {$this->team} AND categoryB ='Moat'";
+	private $rampartsQuery = "SELECT SUM(categoryBScore) AS RampartCrosses FROM match WHERE team = {$this->team} AND categoryB ='Ramparts'";
+	private $drawbridgeQuery = "SELECT SUM(categoryCScore) AS DrawbridgeCrosses FROM match WHERE team = {$this->team} AND categoryC ='Drawbridge'";
+	private $sallyPortQuery = "SELECT SUM(categoryCScore) AS SallyPortCrosses FROM match WHERE team = {$this->team} AND categoryC ='Sally Port'";
+	private $rockWallQuery = "SELECT SUM(categoryDScore) AS RockWallCrosses FROM match WHERE team = {$this->team} AND categoryD ='Rock Wall'";
+	private $roughTerrainQuery = "SELECT SUM(categoryDScore) AS RoughTerrainCrosses FROM match WHERE team = {$this->team} AND categoryD ='Rough Terrain'";
+	private $lowBarQuery = "SELECT SUM(lowBarScore) AS LowBarCrosses FROM match WHERE team = {$this->team}";
+	private $lowGoalHitsQuery = "SELECT SUM(lowGoalShots) AS LowGoalHits FROM match WHERE team = {$this->team}";
+	private $lowGoalMissesQuery = "SELECT SUM(missedLowGoalShots) AS LowGoalMisses FROM match WHERE team = {$this->team}";
+	private $highGoalHitsQuery = "SELECT SUM(highGoalShots) AS HighGoalHits FROM match WHERE team = {$this->team}";
+	private $highGoalMissesQuery = "SELECT SUM(highGoalShots) AS HighGoalMisses FROM match WHERE team = {$this->team}";
+	private $numMatchesQuery = "SELECT team FROM match WHERE team = {$this->team}";
+	private $towerScaleQuery = "SELECT scaleTower FROM match WHERE team = {$this->team} AND scaleTower = 'Yes'";
+	private $towerChallengeQuery = "SELECT challengeTower FROM match WHERE team = {$this->team} AND challengeTower = 'Yes'";
+	private $commentsQuery = "SELECT comments FROM match WHERE team = {$this->team} AND comments != 'N/A'";
 
 
 	// Searching Database for what we need //
 
-	private $portcullisSearch = @mysqli_query($dbc, $portcullisQuery);
-	private $chevalDeFriseSearch = @mysqli_query($dbc, $chevalDeFriseQuery);
-	private $moatSearch = @mysqli_query($dbc, $moatQuery);
-	private $rampartsSearch = @mysqli_query($dbc, $rampartsQuery);
-	private $drawbridgeSearch = @mysqli_query($dbc, $drawbridgeQuery);
-	private $sallyPortSearch = @mysqli_query($dbc, $sallyPortQuery);
-	private $rockWallSearch = @mysqli_query($dbc, $rockWallQuery);
-	private $roughTerrainSearch = @mysqli_query($dbc, $roughTerrainQuery);
-	private $lowBarSearch = @mysqli_query($dbc, $lowBarQuery);
+	public $portcullisSearch = @mysqli_query($dbc, $this->portcullisQuery);
+	public $chevalDeFriseSearch = @mysqli_query($dbc, $this->chevalDeFriseQuery);
+	public $moatSearch = @mysqli_query($dbc, $this->moatQuery);
+	public $rampartsSearch = @mysqli_query($dbc, $this->rampartsQuery);
+	public $drawbridgeSearch = @mysqli_query($dbc, $this->drawbridgeQuery);
+	public $sallyPortSearch = @mysqli_query($dbc, $this->sallyPortQuery);
+	public $rockWallSearch = @mysqli_query($dbc, $this->rockWallQuery);
+	public $roughTerrainSearch = @mysqli_query($this->dbc, $roughTerrainQuery);
+	public $lowBarSearch = @mysqli_query($dbc, $this->lowBarQuery);
 
-	private $lowGoalHitsSearch = @mysqli_query($dbc, $lowGoalHitsQuery);
-	private $lowGoalMissesSearch = @mysqli_query($dbc, $lowGoalMissesQuery);
-	private $highGoalHitsSearch = @mysqli_query($dbc, $highGoalHitsQuery);
-	private $highGoalMissesSearch = @mysqli_query($dbc, $highGoalMissesQuery);
-	private $towerScaleSearch = @mysqli_query($dbc, $towerScaleQuery);
-	private $towerChallengeSearch = @mysqli_query($dbc, $towerChallengeQuery);
+	private $lowGoalHitsSearch = @mysqli_query($dbc, $this->lowGoalHitsQuery);
+	private $lowGoalMissesSearch = @mysqli_query($dbc, $this->lowGoalMissesQuery);
+	private $highGoalHitsSearch = @mysqli_query($dbc, $this->highGoalHitsQuery);
+	private $highGoalMissesSearch = @mysqli_query($dbc, $this->highGoalMissesQuery);
+	private $towerScaleSearch = @mysqli_query($dbc, $this->towerScaleQuery);
+	private $towerChallengeSearch = @mysqli_query($dbc, $this->towerChallengeQuery);
 
-	private $numMatchesSearch = @mysqli_query($dbc, $numMatchesQuery);
-	private $commentsSearch = @mysqli_query($dbc, $commentsQuery);
+	private $numMatchesSearch = @mysqli_query($dbc, $this->numMatchesQuery);
+	private $commentsSearch = @mysqli_query($dbc, $this->commentsQuery);
 
 	//Error Checking
 
 
 	// Search Calculations //
-	if($portcullisSearch && $chevalDeFriseSearch && $moatSearch && $rampartsSearch && 
-		$drawbridgeSearch && $sallyPortSearch && $rockWallSearch && $roughTerrainSearch &&
-		$lowBarSearch && $lowGoalHitsSearch && $lowGoalMissesSearch && $highGoalHitsSearch &&
-		$highGoalMissesSearch && $towerScaleSearch && $noTowerScaleSearch && $numMatchesSearch){
+	if($this->portcullisSearch && $this->chevalDeFriseSearch && $this->moatSearch && $this->rampartsSearch && 
+		$this->drawbridgeSearch && $this->sallyPortSearch && $this->rockWallSearch && $this->roughTerrainSearch &&
+		$this->lowBarSearch && $this->lowGoalHitsSearch && $this->lowGoalMissesSearch && $this->highGoalHitsSearch &&
+		$this->highGoalMissesSearch && $this->towerScaleSearch && $this->noTowerScaleSearch && $this->numMatchesSearch){
 
 		//Scaling / Challengeing Calculations //
-		private $numScale = mysqli_num_rows($towerScaleSearch);
-		private $numberOfMatches = mysqli_num_rows($numMatchesSearch);
-		if($numScale > 0){
-			public $scaleAccuracy = "$numScale / $numberOfMatches"; // Output for Challenge data
+		private $numScale = mysqli_num_rows($this->towerScaleSearch);
+		private $numberOfMatches = mysqli_num_rows($this->numMatchesSearch);
+		if($this->numScale > 0){
+			public $scaleAccuracy = "{$this->numScale}/{$this->numberOfMatches}"; // Output for Challenge data
 		} else {
-			public $scaleAccuracy = "N/A"
+			public $scaleAccuracy = "N/A";
 		}
-		private $numChallenge = mysqli_num_rows($towerChallengeSearch);
-		if($numChallenge > 0){
-			public $challengeAccuracy = "$numChallenge / $numberOfMatches"; // Output for Challenge data
+		private $numChallenge = mysqli_num_rows($this->towerChallengeSearch);
+		if($this->numChallenge > 0){
+			public $challengeAccuracy = "{$this->numChallenge}/{$this->numberOfMatches}"; // Output for Challenge data
 		} else {
-			public $challengeAccuracy = "N/A"
+			public $challengeAccuracy = "N/A";
 		}
 
 		// Low goal Accuracy Calculations //	
-		private $lgha = mysql_fetch_assoc($lowGoalHitsSearch); 
-		public $lowGoalHits = $lgha['LowGoalHits']; // Num low goal shots made
+		private $lgha = mysql_fetch_assoc($this->lowGoalHitsSearch); 
+		public $lowGoalHits = $this->lgha['LowGoalHits']; // Num low goal shots made
 		private $lgma = mysql_fetch_assoc($lowGoalMissesSearch); 
-		private $lowGoalMisses = $lgma['LowGoalMisses'];
-		public $totalLowGoalShots = ($lowGoalHits+$lowGoalMisses);
-		private $lowGoalAccuracy = (($lowGoalHits/$totalLowGoalShots)*100); 
-		public $lowGoalAccuracy = round($lowGoalAccuracy, 1, PHP_ROUND_HALF_UP); //double value of hit precentage
-		// ALSO DISPLAY "$lowGoalHits/$totalLowGoalShots"
+		private $lowGoalMisses = $this->lgma['LowGoalMisses'];
+		public $totalLowGoalShots = ($this->lowGoalHits+$this->lowGoalMisses);
+		private $lowGoalAccuracy = (($this->lowGoalHits/$this->totalLowGoalShots)*100); 
+		private $lowGoalAccuracy = round($this->lowGoalAccuracy, 1, PHP_ROUND_HALF_UP); //double value of hit precentage
+		public $lowGoalAccuracyFraction = "{$this->lowGoalHits}/{$this->totalLowGoalShots}";
+		public $lowGoalAccuracyPercent = "{$this->lowGoalAccuracy}%";
 
 		// High Goal Accuracy Calculations //
-		private$hgha = mysql_fetch_assoc($highGoalHitsSearch); 
-		public$highGoalHits = $lgha['HighGoalHits']; // Num high goal shots made
-		private$hgma = mysql_fetch_assoc($highGoalMissesSearch); 
-		private$highGoalMisses = $lgma['HighGoalMisses'];
-		public$totalHighGoalShots = ($highGoalHits+$highGoalMisses);
-		private$highGoalAccuracy = (($highGoalHits/$totalHighGoalShots)*100); 
-		public$highGoalAccuracy = round($highGoalAccuracy, 1, PHP_ROUND_HALF_UP); //double value of hit precentage
-		// ALSO DISPLAY "$highGoalHits/$totalhighGoalShots"
+		private $hgha = mysql_fetch_assoc($this->highGoalHitsSearch); 
+		public $highGoalHits = $this->lgha['HighGoalHits']; // Num high goal shots made
+		private $hgma = mysql_fetch_assoc($this->highGoalMissesSearch); 
+		private $highGoalMisses = $this->lgma['HighGoalMisses'];
+		public $totalHighGoalShots = ($this->highGoalHits+$this->highGoalMisses);
+		private $highGoalAccuracy = (($this->highGoalHits/$this->totalHighGoalShots)*100); 
+		private $highGoalAccuracy = round($this->highGoalAccuracy, 1, PHP_ROUND_HALF_UP); //double value of hit precentage
+		public $highGoalAccuracyFraction = "{$this->highGoalHits}/{$this->totalhighGoalShots}";
+		public $highGoalAccuracyPercent = "{$this->highGoalAccuracy}%";
 
 		//Defence Varriable's //
 
-		public $portcullis = mysql_fetch_assoc($portcullisSearch);
-		public $chevalDeFrise = mysql_fetch_assoc($chevalDeFriseSearch);
-		public $moat = mysql_fetch_assoc($moatSearch);
-		public $ramparts = mysql_fetch_assoc($rampartsSearch);
-		public $drawbridge = mysql_fetch_assoc($drawbridgeSearch);
-		public $sallyPort= mysql_fetch_assoc($sallyPortSearch);
-		public $rockWall = mysql_fetch_assoc($rockWallSearch);
-		public $roughTerrain = mysql_fetch_assoc($roughTerrainSearch);
-		public $lowBar = mysql_fetch_assoc($lowBarSearch);
+		public $portcullis = mysql_fetch_assoc($this->portcullisSearch);
+		public $chevalDeFrise = mysql_fetch_assoc($this->chevalDeFriseSearch);
+		public $moat = mysql_fetch_assoc($this->moatSearch);
+		public $ramparts = mysql_fetch_assoc($this->rampartsSearch);
+		public $drawbridge = mysql_fetch_assoc($this->drawbridgeSearch);
+		public $sallyPort= mysql_fetch_assoc($this->sallyPortSearch);
+		public $rockWall = mysql_fetch_assoc($this->rockWallSearch);
+		public $roughTerrain = mysql_fetch_assoc($this->roughTerrainSearch);
+		public $lowBar = mysql_fetch_assoc($this->lowBarSearch);
 
 		//todo
 		//num matches calculations
@@ -115,32 +118,10 @@ class teamReport {
 		echo mysqli_error($dbc);
 	}
 };
-
-// wait till it's posted
-if(isset($_POST['search'])){
-
-// Which team is being searched for
-    $redTeam1 = trim($_POST['redTeam1']);
-    $redTeam2 = trim($_POST['redTeam2']);
-    $redTeam3 = trim($_POST['redTeam3']);
-    $blueTeam4 = trim($_POST['blueTeam1']);
-    $blueTeam5 = trim($_POST['blueTeam2']);
-    $blueTeam6 = trim($_POST['blueTeam3']);
-
-// Create team objects //
-// access by obj->$var & obj->$method()
-	$redTeam1Object = new teamReport($redTeam1);
-	$redTeam2Object = new teamReport($redTeam2);
-	$redTeam3Object = new teamReport($redTeam3);
-	$blueTeam1Object = new teamReport($blueTeam1);
-	$blueTeam2Object = new teamReport($blueTeam2);
-	$blueTeam3Object = new teamReport($blueTeam3);
-
-}
-
 // Close connection to the database
 mysqli_close($dbc);
 // mysqli_num_rows()
 //mysqli_fetch_lengths() Returns an array if integer that represents the size of each field (column)
+// mysqli_fetch_array will return a row of data from the query until no further data is available
 
 ?>
