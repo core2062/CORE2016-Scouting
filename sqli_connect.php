7fd073a2948cc6ccc1e6d33ad86027be3fb13222
Brett Diedrich <bdiedric000@stu.waukesha.k12.wi.us>
2016-03-19 14:46:21 -0500

    Organized All Project Files

--- sqli_connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$dbc = @mysqli_connect('localhost', 'root', 'password', 'ScoutagyDev')
-OR die('Could not connect to MySQL: ' .
-mysqli_connect_error());
-
-//echo "<strong>Connected to MySQLi</strong>";
-?>
